4e9272fe230d66872366fb0da0b5e977420f43c8
David Wilhelm <davwilh@gmail.com>
2016-01-20 16:16:30 -0500

    implemented functional GET routes for api

--- routes/api.js
+++ routes/api.js
@@ -4,35 +4,21 @@ const Business = require('./../models/business');
 const Category = require('./../models/category');
 const Street = require('./../models/street');
 const router = express.Router();
+// allows us to reference model name from a string
+const models = {
+  Business,
+  Category,
+  Street
+};
 
 // NOTE: test route
 const test = require('./test');
 
-const api = {
-  fectch(modelName, cb) {
-
-    if (err) cb(err);
-    if (results) cb(null, results);
-    cb();
-  }
-};
-
-function dev() {
-  // NOTE: pseudo code
-  // fetch queries a mongo collection via mongoose model
-  // gets all results from the collection and returns them
-  api.fetch(models[collections.indexOf(collection)], function (err, results) {
-    if (err) res.status(503).send(err);
-    if (results) res.json(results);
-    res.status(503).send('something broke');
-  });
-}
-
 router.route('/').get((req, res) => {
-  res.json({stores: [], streets: [], categories: []});
+  res.sendStatus(404);
 });
 
-// TEST ROUTE
+// NOTE: TEST ROUTE
 router.route('/test')
   .get(test)
   .post((req, res) => {
@@ -42,28 +28,37 @@ router.route('/test')
 router.route('/:collection/:streetName?')
   .get((req, res) => {
     let collection = req.params.collection;
-    let collections = ['stores', 'streets', 'categories'];
-    let models = ['Business', 'Street', 'Category'];
+    let streetName = req.params.streetName;
+    // collectionName: modelName
+    let dbKey = {
+      categories: 'Category',
+      stores: 'Business',
+      streets: 'Street'
+    };
 
     // If the url is unexpected
-    if (collections.indexOf(collection) < 0) return res.sendStatus(404);
+    if (!dbKey[collection]) return res.sendStatus(404);
 
     // if they want to view all the stores for a specific street
-    if (collection === collections[0] && req.params.streetName) {
-      // should be able to pass extra param to api.fetch that lets us
+    if (collection === 'stores' && streetName) {
+      // TODO: should be able to pass extra param to api.fetch that lets us
       // filter query by a prop value
-      res.send('<pre>TODO: implement /stores/'+(req.params.streetName)+' API</pre>');
+      res.send(`<pre>TODO: implement /stores/${streetName} API</pre>`);
       return;
     }
 
     // error if looking for child of collection besides stores
-    if (collection !== collections[0] && req.params.streetName) {
+    if (collection !== 'stores' && streetName) {
       res.sendStatus(404);
       return;
     }
 
-    // If its the correct url
-    res.send('<pre>TODO: implement '+(collection + ' ' || 'the ')+'API</pre>');
+    // If its the correct url query the database
+    models[dbKey[collection]].find((err, results) => {
+      if (err) return res.send(err);
+      if (results) return res.json(results);
+      res.sendStatus(500);
+    });
   })
   .post((req, res) => {
     // TODO
